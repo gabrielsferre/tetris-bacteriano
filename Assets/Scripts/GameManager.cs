f210203bd699fb109a857416d9649d3bf1660427
gabrielsferre <40327850+gabrielsferre@users.noreply.github.com>
2019-01-21 12:51:23 -0200

    Organizador de eventos
    Agora é fácil organizar a ordem em que os eventos do jogo acontecem.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -50,61 +50,6 @@ public class GameManager : MonoBehaviour {
     }
 
     /// <summary>
-    /// Joga bactéria no campo de tetris.
-    /// O argumento 'tempo' define o tempo de espera para que alguma outra
-    /// ação seja feita após a bactéria ser enviada.
-    /// </summary>
-    /// <param name="tempo"></param>
-    /// <returns></returns>
-    private IEnumerator CriaBacteria(float tempo)
-    {
-        grade.CriaBacteria();
-        yield return new WaitForSeconds(1+tempo);
-    }
-
-    /// <summary>
-    /// Transforma em bactéria peças que estejam do lado de bactérias.
-    /// O argumento 'tempo' define o tempo de espera para que alguma outra
-    /// ação seja feita após as peças serem infectadas.
-    /// </summary>
-    /// <param name="tempo"></param>
-    /// <returns></returns>
-    private IEnumerator InfectaPecas(float tempo)
-    {
-        grade.InfectaPecas();
-        yield return new WaitForSeconds(tempo);
-    }
-
-    /// <summary>
-    /// Envia mensagem de um certo tipo (fala ou resposta) para o chat.
-    /// O argumento 'tempo' define o tempo de espera para que alguma outra
-    /// ação seja feita após a mensagem ser enviada.
-    /// </summary>
-    private IEnumerator EnviaMensagem(string mensagem, TipoDeTexto tipo, float tempo)
-    {
-        if( tipo == TipoDeTexto.FALA )
-        {
-            print("Mensagem de fala");
-        }
-        if( tipo == TipoDeTexto.RESPOSTA )
-        {
-            print("Mensagem de resposta");
-        }
-        yield return new WaitForSeconds(tempo);
-    }
-
-    /// <summary>
-    /// Regula nível do medidor de remédio.
-    /// O argumento 'tempo' define o tempo de espera para que alguma outra
-    /// ação seja feita após o medidor ser regulado.
-    /// </summary>
-    private IEnumerator RegulaMedidor(int nivel, float tempo)
-    {
-        print("Medidor regulado");
-        yield return new WaitForSeconds(tempo);
-    }
-
-    /// <summary>
     /// Mantém um loop no jogo de tetris até que um certo
     /// número de peças sejam usadas.
     /// </summary>
@@ -135,12 +80,170 @@ public class GameManager : MonoBehaviour {
     }
 
     /// <summary>
+    /// Yield instruction que lança uma bactéria e depois espera um certo tempo.
+    /// </summary>
+    private class CriaBacteria : CustomYieldInstruction
+    {
+        GameManager gameManager;
+
+        float tempo;    //tempo que a yield instruction irá durar
+        float tempoInicial; //momento em que a yield instruction é criada
+        int nivel;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (Time.realtimeSinceStartup - tempoInicial >= tempo)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public CriaBacteria(GameManager gameManager, float tempo)
+        {
+            this.gameManager = gameManager;
+            this.tempo = tempo;
+            tempoInicial = Time.realtimeSinceStartup;
+
+            gameManager.grade.CriaBacteria();
+        }
+    }
+
+    /// <summary>
+    /// Yield instruction que faz bactérias infectarem peças adjacentes.
+    /// </summary>
+    private class InfectaPecas : CustomYieldInstruction
+    {
+        GameManager gameManager;
+
+        float tempo;    //tempo que a yield instruction irá durar
+        float tempoInicial; //momento em que a yield instruction é criada
+        int nivel;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (Time.realtimeSinceStartup - tempoInicial >= tempo)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public InfectaPecas(GameManager gameManager, float tempo)
+        {
+            this.gameManager = gameManager;
+            this.tempo = tempo;
+            tempoInicial = Time.realtimeSinceStartup;
+
+            gameManager.grade.InfectaPecas();
+        }
+    }
+
+    /// <summary>
+    /// Yield instruction que envia uma mensagem na tela do chat.
+    /// </summary>
+    private class EnviaMensagem : CustomYieldInstruction
+    {
+        GameManager gameManager;
+
+        float tempo;    //tempo que a yield instruction irá durar
+        float tempoInicial; //momento em que a yield instruction é criada
+        TipoDeTexto tipo;
+        string mensagem;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (Time.time - tempoInicial >= tempo)
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public EnviaMensagem(GameManager gameManager, float tempo, TipoDeTexto tipo, string mensagem)
+        {
+            this.gameManager = gameManager;
+            this.tempo = tempo;
+            this.tipo = tipo;
+            this.mensagem = mensagem;
+            tempoInicial = Time.time;
+
+            if (tipo == TipoDeTexto.FALA)
+            {
+                print("Fala: " + mensagem);
+            }
+            if (tipo == TipoDeTexto.RESPOSTA)
+            {
+                print("Resposta: " + mensagem);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Yield instruction que regula o nível do medidor depois espera um certo tempo.
+    /// </summary>
+    private class RegulaMedidor: CustomYieldInstruction
+    {
+        GameManager gameManager;
+
+        float tempo;    //tempo que a yield instruction irá durar
+        float tempoInicial; //momento em que a yield instruction é criada
+        int nivel;
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if( Time.time - tempoInicial >= tempo )
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+        }
+
+        public RegulaMedidor(GameManager gameManager, float tempo, int nivel)
+        {
+            this.gameManager = gameManager;
+            this.tempo = tempo;
+            this.nivel = nivel;
+            tempoInicial = Time.time;
+
+            print("Medidor regulado");
+        }
+    }
+
+    /// <summary>
     /// Controla a ordem dos acontecimentos no jogo
     /// </summary>
     private IEnumerator SequenciaJogo()
     {
-        yield return new LoopTetris(this, 5);
-        print("oi");
+        yield return new LoopTetris(this, 2);
+        yield return new EnviaMensagem(this, 1,TipoDeTexto.FALA, "Estou passando mal");
+        yield return new EnviaMensagem(this, 1, TipoDeTexto.RESPOSTA, "Toma antibiótico");
+        yield return new CriaBacteria(this, 1);
+        yield return new InfectaPecas(this, 1);
         yield return new LoopTetris(this, 4);
     }
 }
